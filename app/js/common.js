7450896a62b0d9ef3fb578b828a6ba6f01294f52
Pangen <pangengit@gmail.com>
2018-12-25 00:25:00 +0300

    Сверстал верхнее меню сайта с адаптивностью, но без поиска

--- app/js/common.js
+++ app/js/common.js
@@ -20,4 +20,11 @@ $(function() {
   $('.search-wrap').click(function(e) {
     e.stopPropagation();
   });
+
+  $('.top-line').after('<div class="mobile-menu d-lg-none">');
+  $('.top-menu').clone().appendTo('.mobile-menu');
+  $('.mobile-menu-button').click(function() {
+    $('.mobile-menu').stop().slideToggle();
+  });
 });
+
